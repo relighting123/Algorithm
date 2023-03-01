f3fd0162ae9873cd5559d3bd957219775849e2d5
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-03-01 15:36:08 +0900

    Time: 916 ms (8.91%), Space: 14.4 MB (51.49%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -6,21 +6,22 @@ class Solution(object):
         :rtype: int
         """
         
-        maxval,minval = abs(sum(nums))+abs(target),(abs(sum(nums))+abs(target))*(-1)
+        maxval,minval = sum(nums),abs(sum(nums))*(-1)
         dp = {}
         
         for t in range(minval,maxval+1):            
-            
-            if t == nums[0] or t == -nums[0]:
-                dp[t]=[1] if nums[0]!=0 else [2]
-            else:
-                dp[t]=[0]
-        
+            dp[t] = [0]
+            if t == nums[0]:
+                dp[t][0] += 1
+            if t == -nums[0]:
+                dp[t][0] += 1
+                
         for i in range(1,len(nums)):
             for t in range(minval,maxval+1):
-                if t-nums[i] < minval or t+nums[i]>maxval:
-                    dp[t].append(0)
-                    continue
-                
-                dp[t].extend([dp[t - (nums[i])][i-1] + dp[t + (nums[i])][i-1]])
+                dp[t].append(0)
+                if t-nums[i] >= minval:
+                    dp[t][-1] += dp[t-nums[i]][i-1]
+                if t+nums[i] <= maxval:
+                    dp[t][-1] += dp[t+nums[i]][i-1]
+        
         return dp[target][-1] if target in dp else 0
