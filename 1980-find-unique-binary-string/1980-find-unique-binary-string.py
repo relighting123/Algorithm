d1a30314574183cd357e5c038e461fea8de88359
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-03-03 04:23:50 +0900

    Time: 36 ms (13.79%), Space: 15.9 MB (5.17%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -4,12 +4,8 @@ class Solution(object):
         :type nums: List[str]
         :rtype: str
         """
-        dic_decimal={}
         dic_decimal=dict(zip([int(str(num),2) for num in nums],[0]*len(nums)))
-        for n in range(2**len(nums)+1):
-            if n not in dic_decimal:
-                return bin(n)[2:].zfill(len(nums))
-                
+        return next(bin(n)[2:].zfill(len(nums)) for n in range(2 ** len(nums) + 1) if n not in dic_decimal)
                         
     
                 
