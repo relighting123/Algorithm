1c975d08a342ba2b11a027cec9705c00972864f3
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-04-10 23:49:05 +0900

    Time: 21 ms (54.47%), Space: 13.3 MB (90.56%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def reverse(self, x):
+        if x==0:
+            return x
+        signed=1
+        max_val= 0
+        if x<0 : 
+            x=x*(-1)
+            max_val = 2**(31)
+            signed=-1
+        else :
+            max_val = 2**(31)-1
+        
+        len_num =int(math.log10(x))
+        ans,len_ans = 0,0
+        val = x// 10**(len_num)
+        i,temp=0,0
+        while len_ans<=len_num:
+            mok= x//10**(len_num-i) 
+            x-=mok*10**(len_num-i)  
+            ans+=mok*10**(i)  
+            len_ans =int(math.log10(ans))
+            if len_num==i and mok ==0:
+                return signed*ans
+            
+            if len_ans==len_num : 
+                ans = signed * ans
+                if -2**(31)<=  ans <2**(31):
+                    return ans
+                else:
+                    return 0
+            i+=1
+        
+        return ans
