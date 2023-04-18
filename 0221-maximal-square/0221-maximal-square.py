298b22a89ec8652ee5381ee802a29c85d9c43c34
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-04-18 23:37:59 +0900

    Time: 656 ms (64.46%), Space: 16.9 MB (87.77%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if not matrix:
+            return 0
+
+        n = len(matrix[0])
+        heights = [0] * (n+1)
+        max_area = 0
+
+        for row in matrix:
+            for i in range(n):
+                if row[i] == "1":
+                    heights[i] += 1
+                else:
+                    heights[i] = 0
+            stack = [-1]
+            for i in range(n+1):
+                while heights[i] < heights[stack[-1]]:
+                    h = heights[stack.pop()]
+                    w = i - stack[-1] - 1
+                    side = min(h, w)
+                    max_area = max(max_area, side*side)
+                stack.append(i)
+
+        return max_area
