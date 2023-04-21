029ec919f271663939dd4bab2ce660851188a926
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-04-21 23:49:53 +0900

    Time: 26 ms (19.95%), Space: 13.4 MB (72.55%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,8 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
     def removeNthFromEnd(self, head, n):
         if head.next == None:
@@ -11,19 +6,28 @@ class Solution(object):
         answer= []    
         point=head
         target = head
-        while n>0:
+        k=n
+        #찾아야할 간격만큼 target을 설정.
+        while k>0:
             target = target.next
-            n-=1
-            
+            k-=1
+        #target이 None인 경우라면 n==노드의 길이. 즉 최초 노드를 제거해야 하는 순간을 의미하므로 head.next를 return함.   
         if target is None:
             return head.next
+
+        if target.next is None:
+            if n == 1 :
+                head.next=None
+                return head
+            else :
+                head.next=head.next.next
+                return head
+        print('aa')
         while target.next:
             answer.append(point)
             point=point.next
            
             target=target.next
-        if len(answer)<1:
-            answer.append(point)
         
         point.next=point.next.next    
         return answer[0]
