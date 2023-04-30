d5e26d34976e2b2e638bc19956e397fef93abf69
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-04-30 17:11:20 +0900

    Time: 469 ms (61.32%), Space: 17 MB (5.65%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,11 +1,10 @@
-from collections import Counter
-
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        task_list = Counter(tasks).values()
-        max_task_cnt = max(task_list)
-        num_distinctmax_cnt = sum(1 for i in task_list if i==max_task_cnt)
-        idle = (n-num_distinctmax_cnt+1)*(max_task_cnt-1)
-        remain_task= sum(task_list)-max_task_cnt*num_distinctmax_cnt  
-        return max(idle,remain_task)+max_task_cnt*num_distinctmax_cnt
- 
+        task_counts = Counter(tasks)
+        max_freq = max(task_counts.values())
+        max_count = sum(1 for count in task_counts.values() if count == max_freq)
+        
+        # Calculate the least number of units of time needed to finish all tasks
+        result = (max_freq - 1) * (n + 1) + max_count
+
+        return max(result, len(tasks))
