c5b8e819756dfc024cd7fec83c62569e4ff6e2dd
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-04-30 17:12:27 +0900

    Time: 729 ms (10.47%), Space: 16.9 MB (5.65%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,10 +1,21 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        task_counts = Counter(tasks)
-        max_freq = max(task_counts.values())
-        max_count = sum(1 for count in task_counts.values() if count == max_freq)
+        task_freq = [(-tasks.count(task), task) for task in set(tasks)]
+        heapq.heapify(task_freq)
         
-        # Calculate the least number of units of time needed to finish all tasks
-        result = (max_freq - 1) * (n + 1) + max_count
-
-        return max(result, len(tasks))
+        intervals = 0
+        while task_freq:
+            i, temp = 0, []
+            while i <= n:
+                intervals += 1
+                if task_freq:
+                    freq, task = heapq.heappop(task_freq)
+                    if freq < -1:
+                        temp.append((freq+1, task))
+                if not task_freq and not temp:
+                    break
+                i += 1
+            for freq, task in temp:
+                heapq.heappush(task_freq, (freq, task))
+        
+        return intervals
