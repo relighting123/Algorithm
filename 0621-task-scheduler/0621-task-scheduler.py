a2cbf5c60e74f51933428a02261ca9303af3f372
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-04-30 17:03:33 +0900

    Time: 729 ms (10.47%), Space: 16.9 MB (5.65%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,21 +1,21 @@
-from collections import defaultdict
-
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        task_freq = defaultdict(int)
-        for task in tasks:
-            task_freq[task] += 1
+        task_freq = [(-tasks.count(task), task) for task in set(tasks)]
+        heapq.heapify(task_freq)
         
-        max_freq = 0
-        num_max_freq = 0
-        for freq in task_freq.values():
-            if freq > max_freq:
-                max_freq = freq
-                num_max_freq = 1
-            elif freq == max_freq:
-                num_max_freq += 1
+        intervals = 0
+        while task_freq:
+            i, temp = 0, []
+            while i <= n:
+                intervals += 1
+                if task_freq:
+                    freq, task = heapq.heappop(task_freq)
+                    if freq < -1:
+                        temp.append((freq+1, task))
+                if not task_freq and not temp:
+                    break
+                i += 1
+            for freq, task in temp:
+                heapq.heappush(task_freq, (freq, task))
         
-        idle_time = (n - num_max_freq + 1) * (max_freq - 1)
-        remaining_tasks = len(tasks) - max_freq * num_max_freq
-        
-        return len(tasks) + max(0, idle_time - remaining_tasks)
+        return intervals
