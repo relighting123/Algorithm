5fec28773ec9d5bd64972fbbabf80ab96fb78a21
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-05-10 00:34:15 +0900

    Time: 769 ms (10.61%), Space: 28.2 MB (10.25%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        satisfaction.sort()
+        min_val = float("-inf")
+        length = len(satisfaction)
+        
+        dp = [[min_val]*length for _ in range(length+1)]
+        
+        #dp[t][dish] = t시점에 dish를 선택시 얻을 수 있는 max Profit
+        dp[0]=[0]*length
+        dp[1]=satisfaction        
+        
+        if length ==1:
+            return max(satisfaction[0],0)
+        
+        for t in range(2,length+1):
+            for dish in range(1,length):
+                dp[t][dish]=max(dp[t][dish],dp[t-1][dish-1])+satisfaction[dish]*t
+        
+        return max(list(zip(*dp))[length-1])  
+            
