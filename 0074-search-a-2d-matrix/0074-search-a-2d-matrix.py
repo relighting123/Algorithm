a9f0cf5cc30af31c57aecdda4e6e4588f2c241e2
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-07-31 11:53:59 +0900

    Time: 53 ms (87.57%), Space: 17 MB (29.06%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -2,7 +2,12 @@ from bisect import bisect_left,bisect_right
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
         n_col,n_row = len(matrix),len(matrix[0])
+        if matrix[0][0]>target or matrix[n_col-1][n_row-1]<target:
+            return False
+        
+        
         col_list=[]
         for i in range(n_col):
             col_list.append(matrix[i][0])
@@ -11,11 +16,8 @@ class Solution:
         if idx_col<len(col_list):
             if col_list[idx_col]==target :
                 return True
-        if idx_col-1<0 :
-            idx_col=1
-        t_list=[]
-        for i in range(n_row):
-            t_list.append(matrix[idx_col-1][i])
+        
+        t_list=matrix[idx_col-1]
 
             
         idx_ans = bisect_left(t_list,target)
@@ -23,7 +25,7 @@ class Solution:
         if idx_ans>=n_row or idx_col>n_col:
             return False
       
-        print(idx_ans)
+       # print(idx_ans)
         if matrix[idx_col-1][idx_ans]==target:
             return True
         else:
