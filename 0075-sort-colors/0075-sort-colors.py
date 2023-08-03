769895c72545e72d37efba617a01fcb8ebf4c0ac
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-08-03 21:01:10 +0900

    Time: 36 ms (97.03%), Space: 16.3 MB (72.10%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,16 +1,27 @@
+from bisect import bisect_left,bisect_right
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i, a, b = 0, 0, len(nums) - 1
+        i,a,b=0,0,len(nums)-1    
 
-        while i <= b:
-            if nums[i] == 0 and i > a:
-                nums[a], nums[i] = nums[i], nums[a]
-                a += 1
-            elif nums[i] == 2 and i < b:
-                nums[b], nums[i] = nums[i], nums[b]
-                b -= 1
+        while(i<=b):
+            if nums[i]==0  and i > a:
+               # print("k",i)
+                nums[a],nums[i]=nums[i],nums[a]
+                a+=1
+                #print(nums)                
+            elif nums[i]==2  and i < b:
+                nums[b],nums[i]=nums[i],nums[b]                
+                b-=1            
             else:
-                i += 1
+                i+=1
+            
+            
+    
+
+            
+    
+        
+        
