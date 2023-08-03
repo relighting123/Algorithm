71a3c580691aed58da3078b8485893d6510a68ba
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-08-03 20:43:19 +0900

    Time: 44 ms (80.66%), Space: 16.4 MB (38.10%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,25 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i,a,b=0,0,len(nums)-1       
-        
-        while(i<=b):
-            if nums[i]==0:
-               # print("k",i)
-                nums[a],nums[i]=nums[i],nums[a]
-                a+=1
-                i+=1
-                #print(nums)                
-            elif nums[i]==2:
-                nums[b],nums[i]=nums[i],nums[b]                
-                b-=1            
-            else:
-                i+=1
-            
-            
-    
+        i, a, b = 0, 0, len(nums) - 1
 
-            
-    
-        
-        
+        while i <= b:
+            if nums[i] == 0:
+                if i != a:  # Avoid unnecessary swap if already in the correct position
+                    nums[a], nums[i] = nums[i], nums[a]
+                a += 1
+                i += 1
+            elif nums[i] == 2:
+                if i != b:  # Avoid unnecessary swap if already in the correct position
+                    nums[b], nums[i] = nums[i], nums[b]
+                b -= 1
+            else:
+                i += 1
