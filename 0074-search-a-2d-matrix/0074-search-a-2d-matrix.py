c9978d4033ae50ed978b5c6ad197c1dc20c6e510
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-08-03 22:29:58 +0900

    Time: 56 ms (74.23%), Space: 17 MB (28.98%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -4,16 +4,16 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
         n_col,n_row = len(matrix),len(matrix[0])
+        #최솟값,최대값 기준에 존재할 수 없는 경우 정의
         if matrix[0][0]>target or matrix[n_col-1][n_row-1]<target:
             return False
         
-        
+        #첫번째 열을 행 배열로 전환하여 binary Search 대상 선정. 이를 통해 몇번째 행이 중요한지 확인
         col_list=[row[0] for row in matrix]
                 
         idx_col = bisect_left(col_list,target)
-        if idx_col<len(col_list):
-            if col_list[idx_col]==target :
-                return True
+        if idx_col<len(col_list) and col_list[idx_col]==target:
+            return True
         
         t_list=matrix[idx_col-1]
             
