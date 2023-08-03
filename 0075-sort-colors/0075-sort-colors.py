4ea8f8e7ff5df31094f73d39dda4256630bb6736
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-08-03 20:54:58 +0900

    Time: 35 ms (98.30%), Space: 16.4 MB (38.10%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,19 +1,37 @@
+from bisect import bisect_left,bisect_right
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i, a, b = 0, 0, len(nums) - 1
-
-        while i <= b:
-            if nums[i] == 0:
-                if i != a:  # Avoid unnecessary swap if already in the correct position
-                    nums[a], nums[i] = nums[i], nums[a]
-                a += 1
-                i += 1
-            elif nums[i] == 2:
-                if i != b:  # Avoid unnecessary swap if already in the correct position
-                    nums[b], nums[i] = nums[i], nums[b]
-                b -= 1
+        i,a,b=0,0,len(nums)-1    
+        #속도 개선(불필요한 Swap 최소화)
+        
+        max_val,min_val = max(nums),min(nums)
+        i=bisect_left(nums,min_val)
+        a=i
+        b=bisect_right(nums,max_val)-1
+        
+        print(a,b)
+        
+        
+        while(i<=b):
+            if nums[i]==0:
+               # print("k",i)
+                nums[a],nums[i]=nums[i],nums[a]
+                a+=1
+                i+=1
+                #print(nums)                
+            elif nums[i]==2:
+                nums[b],nums[i]=nums[i],nums[b]                
+                b-=1            
             else:
-                i += 1
+                i+=1
+            
+            
+    
+
+            
+    
+        
+        
