c6490c6295d94cc9d9953b5e9199bf44785ad67e
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-08-21 13:25:05 +0900

    Time: 49 ms (63.24%), Space: 16.7 MB (14.69%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m,n,dp =len(obstacleGrid),len(obstacleGrid[0]), {}
+        m,n,dp =len(obstacleGrid),len(obstacleGrid[0]), collections.defaultdict(int)
         dp[(0,0)]=1
         for row in range(m):
             for col in range(n):
@@ -9,15 +9,7 @@ class Solution:
                     continue
                 if row==0 and col==0:
                     dp[(0,0)]=1
-                    continue
-                if row==0 and col>0:
-                    dp[(row,col)]=dp[(row,col-1)]
-                    continue
-                if col==0 and row>0:
-                    dp[(row,col)]=dp[(row-1,col)]
-                    continue
-                
-                    
+                    continue                   
                     
                 dp[(row,col)]=dp[(row-1,col)]+dp[(row,col-1)]
         return dp[(m-1,n-1)]
