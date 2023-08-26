6487c93429fc70f76ffe1cfe5a9a1a2cfa055a9b
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-08-26 14:06:08 +0900

    Time: 1395 ms (18.12%), Space: 31.4 MB (28.00%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+
+        if len(nums)<=1:
+            return 0
+        
+        #Radix Sort를 통한 정렬
+        buckets = [deque() for _ in range(10)]
+        queue = deque(nums)
+        max_num = max(nums)
+        factor=1
+        
+        while max_num>=factor:
+            while queue:
+                num = queue.popleft()
+                buckets[num//factor%10].append(num)
+            
+            for bucket in buckets:
+                while bucket:
+                    queue.append(bucket.popleft())
+            
+            factor *=10
+        nums=list(queue)
+        print(nums)
+        
+        #인접 두개 요소 간 차이 계산
+        l,ans=0,abs(nums[1]-nums[0])
+        while l<len(nums)-1:
+            ans = max(nums[l+1]-nums[l],ans)
+            l+=1
+        return ans
