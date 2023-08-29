579b34cd66249b196fbc2649bf07a3e36d07364f
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-08-29 23:05:00 +0900

    Time: 153 ms (57.75%), Space: 19.6 MB (10.22%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,14 +1,16 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        def empty_queue(queue,cnt_prevCandy,ans):
-            j,n=1,len(queue)
-            while queue:
-                ans+= j
-                queue.popleft()
-                j+=1
-            ans-=min(cnt_prevCandy,n)
-            queue=deque([])
+        def empty_queue(queue, cnt_prevCandy, ans):
+            n = len(queue)
+            print(queue,n)
+            ans += int((1 + n) * n / 2)
+            ans -= min(cnt_prevCandy, n)
+            # while queue:
+            #     ans+= j
+            #     queue.popleft()
+            #     j+=1
             return ans
+
         
         
         queue,i,n,cnt_prevCandy,ans=deque(),0,len(ratings),0,0 
@@ -22,7 +24,8 @@ class Solution:
                     ans+=1
                 if queue:
                     queue.append(cur_val)
-                    ans=empty_queue(queue,cnt_prevCandy,ans)  
+                    ans=empty_queue(queue,cnt_prevCandy,ans)
+                    queue = deque([])
                     cnt_prevCandy=1
                     
             #오름차순인 경우 i-1번쨰 캔디 갯수를 을 list에 저장함. 캔디 갯수를 이전 갯수 +1 진행함.
@@ -36,6 +39,7 @@ class Solution:
                 if i==n-1:
                     queue.append(cur_val)
                     ans=empty_queue(queue,cnt_prevCandy,ans)  
+                    queue = deque([])
                     cnt_prevCandy=1
                 
         return ans if n>1 else 1
