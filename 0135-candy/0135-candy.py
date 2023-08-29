ef09938fdea0d98ab090aaa466db752a1a728331
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-08-29 22:48:53 +0900

    Time: 163 ms (46.87%), Space: 19.3 MB (40.35%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -17,33 +17,29 @@ class Solution:
         
         for i in range(1,n):
             prev_val,cur_val=ratings[i-1],ratings[i]
-            if cur_val>=prev_val and i==1:
-                cnt_prevCandy=1
-                ans+=1
-            #오름차순인 경우 i-1번쨰 캔디 갯수를 을 list에 저장함. 캔디 갯수를 이전 갯수 +1 진행함.
-            if cur_val>prev_val:
+            if cur_val>=prev_val:
+                if i==1:
+                    cnt_prevCandy=1
+                    ans+=1
                 if queue:
                     queue.append(cur_val)
                     ans=empty_queue(queue,cnt_prevCandy,ans)  
                     cnt_prevCandy=1
-                
+                    
+            #오름차순인 경우 i-1번쨰 캔디 갯수를 을 list에 저장함. 캔디 갯수를 이전 갯수 +1 진행함.
+            if cur_val>prev_val:
                 cnt_prevCandy= cnt_prevCandy+1
                 ans+=cnt_prevCandy
                 
             #내림차순인 경우 
             elif cur_val<prev_val:
-
                 queue.append(prev_val)
                 if i==n-1:
                     queue.append(cur_val)
                     ans=empty_queue(queue,cnt_prevCandy,ans)  
                     cnt_prevCandy=1
             #전/후 동일한 경우 내림차순 queue 내 값을 활용하여 list_ans 업데이트하고 초기화 flag를 전달함.
-            else :
-                if queue:
-                    queue.append(cur_val)
-                    ans=empty_queue(queue,cnt_prevCandy,ans)  
-                    cnt_prevCandy=1
+            else :       
                 ans+=1
                 cnt_prevCandy=1
         if n==1:
