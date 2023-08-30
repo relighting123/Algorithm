0269405ac89f8fe6a483395a032b0cb1054633e3
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-08-30 22:04:29 +0900

    Time: 151 ms (58.97%), Space: 19.6 MB (10.31%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,10 +1,10 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        def empty_queue(queue, cnt_prevCandy, ans):
+        def empty_queue(queue, cnt_Candy, ans):
             n = len(queue)
             print(queue,n)
             ans += int((1 + n) * n / 2)
-            ans -= min(cnt_prevCandy, n)
+            ans -= min(cnt_Candy, n)
             # while queue:
             #     ans+= j
             #     queue.popleft()
@@ -13,33 +13,33 @@ class Solution:
 
         
         
-        queue,i,n,cnt_prevCandy,ans=deque(),0,len(ratings),0,0 
+        queue,i,n,cnt_Candy,ans=deque(),0,len(ratings),0,0 
         
         
         for i in range(1,n):
             prev_val,cur_val=ratings[i-1],ratings[i]
             if cur_val>=prev_val:
                 if i==1:
-                    cnt_prevCandy=1
+                    cnt_Candy=1
                     ans+=1
                 if queue:
                     queue.append(cur_val)
-                    ans=empty_queue(queue,cnt_prevCandy,ans)
+                    ans=empty_queue(queue,cnt_Candy,ans)
                     queue = deque([])
-                    cnt_prevCandy=1
+                    cnt_Candy=1
                     
-            #오름차순인 경우 i-1번쨰 캔디 갯수를 을 list에 저장함. 캔디 갯수를 이전 갯수 +1 진행함.
             if cur_val>=prev_val:
-                cnt_prevCandy= cnt_prevCandy+1 if cur_val>prev_val else 1
-                ans+=cnt_prevCandy
+                cnt_Candy= cnt_Candy+1 if cur_val>prev_val else 1
+                ans+=cnt_Candy
+                continue
                 
-            #내림차순인 경우 
-            elif cur_val<prev_val:
+            if cur_val<prev_val:
                 queue.append(prev_val)
                 if i==n-1:
                     queue.append(cur_val)
-                    ans=empty_queue(queue,cnt_prevCandy,ans)  
+                    ans=empty_queue(queue,cnt_Candy,ans)  
                     queue = deque([])
-                    cnt_prevCandy=1
+                    cnt_Candy=1
+                continue
                 
         return ans if n>1 else 1
