65d4c9cd0bfdaab48ee7c0efd69f94d99ed2ebb0
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-08-30 22:17:44 +0900

    Time: 138 ms (88.14%), Space: 19.3 MB (69.11%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,45 +1,14 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        def empty_queue(queue, cnt_Candy, ans):
-            n = len(queue)
-            print(queue,n)
-            ans += int((1 + n) * n / 2)
-            ans -= min(cnt_Candy, n)
-            # while queue:
-            #     ans+= j
-            #     queue.popleft()
-            #     j+=1
-            return ans
-
+        N = len(ratings)
+        nums = [1]*N
         
+        for n in range(1, N):
+            if ratings[n-1] < ratings[n]:
+                nums[n] = nums[n-1] + 1
         
-        queue,i,n,cnt_Candy,ans=deque(),0,len(ratings),0,0 
-        
-        
-        for i in range(1,n):
-            prev_val,cur_val=ratings[i-1],ratings[i]
-            if cur_val>=prev_val:
-                if i==1:
-                    cnt_Candy=1
-                    ans+=1
-                if queue:
-                    queue.append(cur_val)
-                    ans=empty_queue(queue,cnt_Candy,ans)
-                    queue = deque([])
-                    cnt_Candy=1
-                    
-            if cur_val>=prev_val:
-                cnt_Candy= cnt_Candy+1 if cur_val>prev_val else 1
-                ans+=cnt_Candy
-                continue
+        for n in range(N-1, 0, -1):
+            if ratings[n-1] > ratings[n]:
+                nums[n-1] = max(nums[n-1], nums[n] + 1)
                 
-            if cur_val<prev_val:
-                queue.append(prev_val)
-                if i==n-1:
-                    queue.append(cur_val)
-                    ans=empty_queue(queue,cnt_Candy,ans)  
-                    queue = deque([])
-                    cnt_Candy=1
-                continue
-                
-        return ans if n>1 else 1
+        return sum(nums)
