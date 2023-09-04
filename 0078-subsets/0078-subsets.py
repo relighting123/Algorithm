f0b94a71369c440269814fb684e2df76c25cf950
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-09-04 20:58:06 +0900

    Time: 39 ms (79.45%), Space: 16.5 MB (42.58%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,17 +1,18 @@
+from typing import List
+from collections import deque
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans=[[]]
-
-        n=len(nums)
-        def dfs(start,subans):
-            ans.append(subans)
-            for i in range(1,len(nums)):
-                if start+i>=len(nums):
-                    break
-                dfs(start+i,subans+[nums[start+i]])
-            
-        for i in range(len(nums)):
-            dfs(i,[nums[i]])
-        return ans
-
+        n = len(nums)
+        subsets = []
         
+        def backtrack(start, current_subset):
+            subsets.append(current_subset[:])  # 현재 부분 집합을 결과에 추가
+            
+            for i in range(start, n):
+                current_subset.append(nums[i])  # 요소를 현재 부분 집합에 추가
+                backtrack(i + 1, current_subset)  # 다음 요소를 선택하도록 재귀 호출
+                current_subset.pop()  # 현재 요소를 제거하여 다른 조합을 시도
+        
+        backtrack(0, [])
+        return subsets
