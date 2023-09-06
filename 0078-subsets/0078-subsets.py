d8c1303b2b8285d8ba64f3b0684df55162d750d9
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-09-06 13:52:16 +0900

    Time: 48 ms (28.76%), Space: 16.5 MB (75.43%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,13 @@
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans,n=[],2**len(nums)
+        ans,n=[[]],2**len(nums)
         for i in range(n):
             subans=[]
-            for j in range(len(nums)):
-                 if i&1<<j :
+            for j in range(min(i,len(nums))):
+                if i&1<<j :
                     subans.append(nums[j])
-            ans.append(subans)
+                if subans not in ans:
+                    ans.append(subans)
         return ans
         
-    
