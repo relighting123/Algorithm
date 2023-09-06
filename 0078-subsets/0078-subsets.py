244fcebde4077645a2ec13128e2dce3348d5d929
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-09-06 10:55:36 +0900

    Time: 3658 ms (5.07%), Space: 16.5 MB (75.43%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,18 +1,14 @@
-from typing import List
-from collections import deque
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        subsets = []
+        ans,subans,n=[],[],2**len(nums)
+        for i in range(n):
+            subans=[]
+            for j in range(n):
+                if i&1<<j :
+                    subans.append(nums[j])
+                if subans not in ans:
+                    ans.append(subans)
+        return ans
         
-        def backtrack(start, current_subset):
-            subsets.append(current_subset[:])  # 현재 부분 집합을 결과에 추가
+    
             
-            for i in range(start, n):
-                current_subset.append(nums[i])  # 요소를 현재 부분 집합에 추가
-                backtrack(i + 1, current_subset)  # 다음 요소를 선택하도록 재귀 호출
-                current_subset.pop()  # 현재 요소를 제거하여 다른 조합을 시도
-        
-        backtrack(0, [])
-        return subsets
