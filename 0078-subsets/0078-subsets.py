ca476b363add6c2f69e0d9b75fe1eaae3b751b21
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-09-06 10:55:41 +0900

    Time: 53 ms (13.51%), Space: 16.5 MB (42.46%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -3,7 +3,7 @@ class Solution:
         ans,subans,n=[],[],2**len(nums)
         for i in range(n):
             subans=[]
-            for j in range(n):
+            for j in range(len(nums)):
                 if i&1<<j :
                     subans.append(nums[j])
                 if subans not in ans:
