c91d23248912825cb8a6f3ade6b72a664a491bb3
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-09-06 13:50:13 +0900

    Time: 36 ms (87.21%), Space: 16.6 MB (9.60%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        ans = []
-        
-        # 비트마스킹을 이용하여 부분집합 생성
-        for i in range(2**n):
-            subset = []
-            for j in range(n):
+        ans,n=[],2**len(nums)
+        for i in range(n):
+            subans=[]
+            for j in range(len(nums)):
+               
                 if (i >> j) & 1:
-                    subset.append(nums[j])
-            ans.append(subset)
-        
+                    subans.append(nums[j])
+            ans.append(subans)
         return ans
+        
+    
