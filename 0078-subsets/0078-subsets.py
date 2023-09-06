4329a66ca4f5b589b768497609d511ee384a060f
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-09-06 13:53:52 +0900

    Time: 43 ms (57.19%), Space: 16.5 MB (75.43%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -4,9 +4,7 @@ class Solution:
         for i in range(n):
             subans=[]
             for j in range(min(i,len(nums))):
-                 if i&1<<j :
+                if i&1<<j :
                     subans.append(nums[j])
             ans.append(subans)
         return ans
-        
-    
