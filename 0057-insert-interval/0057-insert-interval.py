c2f5f04bfc03db1cf86102fc15213fdf3c18a8d8
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-09-16 22:07:20 +0900

    Time: 90 ms (30.53%), Space: 19.7 MB (77.30%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -5,13 +5,7 @@ class Solution:
         s2=bisect_left([item[1] for item in intervals],newInterval[0])
         e1=bisect_right([item[0] for item in intervals],newInterval[1])
         e2=bisect_right([item[1] for item in intervals],newInterval[1])
-        print(s1,s2,e1,e2)
-        
-        #case1. s1,s2,e1,e2가 모두 동일한 경우 s1 인덱스에 new Intervals을 insert 처리
-        #case2. s1-s2==1이면 s2인덱스의 범위 내 newintervals의 end point가 위치함.
-        #case3. e1-e2==1이면 e2인덱스의 범위 내 newintervals의 end point가 위치함.
-        #case4. s1==s2이면 s1인덱스의 시작점보다 newintervals의 시작점이 작아 시작점대체 가능
-        #case5. e1==e2이면 e1-1의 인덱스의 끝점보다 newintervals의 end point가 더 커서 끝점 대체 가능
+      
         
         if s1==s2==e1==e2:
             return intervals[:s1]+[newInterval]+intervals[s1:]
