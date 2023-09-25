a9076cfc471da31d822f236d3c09ca1ac3a9f57f
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-09-26 00:13:14 +0900

    Time: 31 ms (96.74%), Space: 16.2 MB (83.71%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,34 +1,16 @@
+from collections import deque
+
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        ans = deque()
-        def searchFile(queuepath):
-            queue = queuepath
-            File = ""
-            File+=(queue.popleft())
-
-            while queue:
-                if queue[0]=="/":
-                    break
-               
-                File+=(queue.popleft())
-            return (queue),(File)
+        stack = deque()
+        parts = path.split('/')  # 경로를 슬래시로 분할
         
-        queue=deque(path)
-        ans=""
-        FileMemory = deque()
-        while queue:
-            queue,nextFile = searchFile(queue)
-            if nextFile =="/..":
-                currFile=FileMemory.pop() if FileMemory else ""               
-                ans=ans[:-len(currFile)]
-            elif nextFile == "/." or nextFile == "/" :
-                continue
-            else:
-                ans+=nextFile
-                FileMemory.append(nextFile)
-        return ans if len(ans)>0 else "/"
-            
-            
-            
-            
-             
+        for part in parts:
+            if part == '..':
+                if stack:
+                    stack.pop()  # 상위 디렉토리로 이동
+            elif part and part != '.':
+                stack.append(part)  # 현재 디렉토리 추가
+        
+        simplified_path = '/' + '/'.join(stack)
+        return simplified_path if simplified_path else '/'
