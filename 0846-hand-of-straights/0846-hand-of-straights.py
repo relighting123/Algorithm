a622fe85017f5e3cd0bd70499a84c8cb55aaaec0
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-10-05 22:23:07 +0900

    Time: 163 ms (98.39%), Space: 17.7 MB (96.98%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,23 +1,21 @@
-
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         n=len(hand)
         if groupSize ==1: return True
         if n%groupSize != 0: return False
         
-        l,r,visited,cnt,cntgroup =0,0,[True]+[False]*(n-1),1,0
+        l,r,visited,cnt,cnt_batch =0,0,[True]+[False]*(n-1),1,0
         hand.sort()
         prevVal=hand[l]
         while r<n:
             if visited[r] or hand[r] == prevVal: r += 1; continue
             if hand[r]>prevVal+1 and r!=l: return False
-            
             visited[l],visited[r],prevVal=True,True,hand[r]            
             r+=1
             cnt+=1
             if cnt==groupSize:
-                cntgroup+=1
-                if cntgroup*cnt==n: return True  
+                cnt_batch+=1
+                if cnt_batch*cnt==n: return True  
                 while visited[l]: l+=1
                 visited[l]=True
                 r=l
