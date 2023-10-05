bc8420901b157019eae4335ea727a0e35fe46b75
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-10-05 21:47:25 +0900

    Time: 324 ms (16.23%), Space: 17.8 MB (91.29%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -10,24 +10,19 @@ class Solution:
         prevVal=hand[l]
         while r<n:
             if visited[r] or hand[r]==prevVal or r==l: 
-                #print("r은 이미 방문했거나 이전 값과 동일 값입니다. 위치 +1합니다.")
                 if r==l:
                     prevVal=hand[l]
                 r+=1
-
                 continue
             if hand[r]>prevVal+1 and prevVal!=-1:
-                #print("현재 그룹 내 찾은 수는 ",cnt,"입니다", "groupsize 도달 전 연속 수가 아니므로 False 처리합니다")
                 return False
             
             visited[l],visited[r]=True,True
             prevVal=hand[r]
             r+=1
             cnt+=1
-            if all(visited) and cnt==groupSize:
-                #print("return True")
-                return True
             if cnt==groupSize:
+                if all(visited) : return True
                 #print("get",visited)
                 cnt,prevVal=1,-1
                 while visited[l]:
