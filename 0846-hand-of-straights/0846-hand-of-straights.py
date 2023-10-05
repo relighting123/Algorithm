e7d900c3019f491d48473c3076424aeefa7b5268
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-10-05 20:01:30 +0900

    Time: 1141 ms (6.13%), Space: 18.1 MB (43.94%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        n=len(hand)
+        if groupSize ==1:
+            return True
+        if n%groupSize != 0:
+            return False
+        
+        dic=defaultdict(int)
+        hand.sort()
+       
+        for i in range(n):
+            dic[hand[i]]+=1
+        cnt=0
+        while cnt<=n:
+            if cnt==n:
+                return True
+            for i in dic:
+                if dic[i]>0:
+                    s=i
+                    print(s)
+                    break
+            for i in range(groupSize):
+                if dic[s+i]==0:
+                    return False
+                dic[s+i]-=1
+                cnt+=1
+        return True
+            
+                
+        
