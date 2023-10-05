aad427abe7e5951ffee846a5c5be658534bd7092
relighting123 <63687478+relighting123@users.noreply.github.com>
2023-10-05 22:21:58 +0900

    Time: 162 ms (98.70%), Space: 17.7 MB (96.98%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -5,21 +5,19 @@ class Solution:
         if groupSize ==1: return True
         if n%groupSize != 0: return False
         
-        l,r,visited,cnt =0,0,[True]+[False]*(n-1),1
+        l,r,visited,cnt,cntgroup =0,0,[True]+[False]*(n-1),1,0
         hand.sort()
         prevVal=hand[l]
         while r<n:
-            if visited[r] or hand[r] == prevVal: 
-                r += 1;
-                continue
+            if visited[r] or hand[r] == prevVal: r += 1; continue
             if hand[r]>prevVal+1 and r!=l: return False
             
             visited[l],visited[r],prevVal=True,True,hand[r]            
             r+=1
             cnt+=1
             if cnt==groupSize:
-                if all(visited) : return True
-              
+                cntgroup+=1
+                if cntgroup*cnt==n: return True  
                 while visited[l]: l+=1
                 visited[l]=True
                 r=l
