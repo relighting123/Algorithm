4262a9d60b5383bc21bc90486d6f8418dd3a8776
relighting123 <63687478+relighting123@users.noreply.github.com>
2024-04-04 00:27:05 +0900

    Time: 51 ms (15.34%), Space: 18.3 MB (6.05%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -10,22 +10,18 @@ class Solution:
             char = s[i]
             que.append((char,i))
             if len(que)>=2:
-                #print("cc")
                 a=que.pop()
                 b=que.pop()
-               # print(b+a)
                 if b[0]+a[0]!='()':
                     que.append(b)
                     que.append(a)
                 continue
-        print(que)
         lastidx=len(s)-1
         if len(que)==0:
             return len(s)
     
         while que:
             idx=que.pop()
-            print(ans,lastidx,idx[1])
             val=lastidx-idx[1]
             ans=max(ans,val)
             lastidx=idx[1]
