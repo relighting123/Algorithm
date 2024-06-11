b6918637eed2049b5520c943f5296a0790aee424
relighting123 <63687478+relighting123@users.noreply.github.com>
2024-06-11 23:59:50 +0900

    Time: 99 ms (23.04%), Space: 21.4 MB (30.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,15 @@
 from collections import defaultdict
-from typing import List
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         def convert(string):
             base = [0] * 26
             for char in string:
-                base[ord(char) - ord('a')] += 1
+                idx = ord(char) - ord('a')
+                base[idx] += 1
             return tuple(base)
         
         dict_a = defaultdict(list)
+        
         for string in strs:
             dict_a[convert(string)].append(string)
         
